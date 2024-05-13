ff95ca50d896200bb934b2a5f47672ac2d68df4d
cole <garcia.ccole@gmail.com>
2024-05-13 14:52:02 +0800

    hayz

--- app/api/voltages/route.ts
+++ app/api/voltages/route.ts
@@ -1,7 +1,6 @@
 import { VoltageModel } from "@/models/Voltage";
 import { NextResponse } from "next/server";
 
-export const fetchCache = "fore-no-store";
 export const GET = async () => {
   const voltageData = await VoltageModel.find({});
   return NextResponse.json(voltageData);
