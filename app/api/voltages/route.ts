38ed352de195cadb17c1b33111aa5ea83787babb
cole <garcia.ccole@gmail.com>
2024-05-13 14:14:25 +0800

    try

--- app/api/voltages/route.ts
+++ app/api/voltages/route.ts
@@ -1,6 +1,7 @@
 import { VoltageModel } from "@/models/Voltage";
 import { NextResponse } from "next/server";
-export const dynamic = "force-dynamic";
+
+export const revalidate = 1; //revalidate api every 1 second
 
 export const GET = async () => {
   const voltageData = await VoltageModel.find({});
