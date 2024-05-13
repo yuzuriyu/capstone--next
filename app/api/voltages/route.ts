96bcecc7554c81d49f0831719c2b55afe462e546
cole <garcia.ccole@gmail.com>
2024-05-13 14:44:44 +0800

    try

--- app/api/voltages/route.ts
+++ app/api/voltages/route.ts
@@ -1,9 +1,8 @@
 import { VoltageModel } from "@/models/Voltage";
 import { NextResponse } from "next/server";
-import { revalidatePath } from "next/cache";
 
+export const dynamic = "force-dynamic";
 export const GET = async () => {
   const voltageData = await VoltageModel.find({});
-  revalidatePath(voltageData);
   return NextResponse.json(voltageData);
 };
