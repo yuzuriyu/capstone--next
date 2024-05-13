901fe291f04e444ed0a79044fe142d16c33b5500
cole <garcia.ccole@gmail.com>
2024-05-13 14:46:23 +0800

    try

--- app/api/voltages/route.ts
+++ app/api/voltages/route.ts
@@ -1,7 +1,7 @@
 import { VoltageModel } from "@/models/Voltage";
 import { NextResponse } from "next/server";
 
-export const dynamic = "force-dynamic";
+export const fetchCache = "fore-no-store";
 export const GET = async () => {
   const voltageData = await VoltageModel.find({});
   return NextResponse.json(voltageData);
