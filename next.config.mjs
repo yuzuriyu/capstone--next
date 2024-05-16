380de85aef58b0611eb6e10608f9d8fe914d44b0
cole <garcia.ccole@gmail.com>
2024-05-17 07:19:13 +0800

    try cache control

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
         // Allow CORS for all API routes
         source: "/api/:path*",
         headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=1, stale-while-revalidate=59",
+          },
           { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
