f35216966914ba91afd2e9d5a284df14ef089475
cole <garcia.ccole@gmail.com>
2024-05-17 09:03:50 +0800

    hayz

--- app/api/voltages/route.ts
+++ app/api/voltages/route.ts
@@ -1,9 +1,7 @@
 import { VoltageModel } from "@/models/Voltage";
-import { revalidateTag } from "next/cache";
 import { NextResponse } from "next/server";
 
 export const GET = async () => {
   const voltageData = await VoltageModel.find({});
-  revalidateTag("voltage");
-  return NextResponse.json({ voltageData, revalidated: true, now: Date.now() });
+  return NextResponse.json(voltageData);
 };
